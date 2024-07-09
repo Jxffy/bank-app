5147d22387bbe870affdc47475f9be65d26425b1
Sam Jeffy <144205599+Jxffy@users.noreply.github.com>
2024-07-09 19:24:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("admin").onclick=function(){
+    document.getElementById("login-details").style.display="block";
+}
+
+
+document.getElementById("candy").onclick=function(){
+    document.getElementById("login-details").style.display="block";
+}
+
+
+
+document.getElementById("closebtn-1").onclick=function(){
+    document.getElementById("login-details").style.display="none";
+}
+
