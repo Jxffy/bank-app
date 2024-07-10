8b07930bab4cfd965f5621525f82ec42751b01c0
Sam Jeffy <144205599+Jxffy@users.noreply.github.com>
2024-07-10 07:10:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//login page//
+
 document.getElementById("admin").onclick=function(){
     document.getElementById("login-details").style.display="block";
 }
@@ -13,3 +15,20 @@ document.getElementById("closebtn-1").onclick=function(){
     document.getElementById("login-details").style.display="none";
 }
 
+
+
+//form//
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Initialize multiselect
+    var multiselectElements = document.querySelectorAll('.multiselect');
+    multiselectElements.forEach(function(element) {
+      new bootstrapMultiselect(element);
+    });
+  
+    // Initialize datepicker
+    var datepickerElements = document.querySelectorAll('.datepicker');
+    datepickerElements.forEach(function(element) {
+      new bootstrapDatepicker(element);
+    });
+  });
